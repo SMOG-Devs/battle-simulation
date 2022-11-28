243b8f171e097bd509f5a5ff3358db08b7ff599a
Bartosz Sambór <72545684+BartoszSambor@users.noreply.github.com>
2022-11-29 00:19:05 +0100

    First unit

---
+++ src/Agent/units.py
@@ -0,0 +1,44 @@
+import agentpy as ap
+import random
+
+
+class Soldier(ap.Agent):
+
+    def setup_map_binding(self, battle_front: ap.Grid):
+        self.battle_front = battle_front
+
+
+    def setup(self):
+        # team:
+        # 'blue' -> 0
+        # 'red' -> 1
+        self.team = 0
+        # Initialize an attribute with a parameter
+        self.health = 100
+        self.damage = 200  # To kill enemy in one step for easier debugging
+
+        # status:
+        #   2 - alive and fighting (default)
+        #   1 - surrendered and running away
+        #   0 - dead
+        self.status = 2
+
+        # view range
+        self.view_range = 2
+
+        # self.endurance = 100
+        # self.strength = 100
+        # self.speed = 100
+        # self.itd = 123
+        # self.itp = 321
+
+
+    def attack(self, enemy):
+        enemy.health -= self.damage
+        # if soldier killed enemy
+        if enemy.health <= 0:
+            enemy.status = 0
+
+    # TODO: One soldiers shouldn't be on top of another
+    def move(self, x_axis: int, y_axis: int):
+        self.battle_front.move_by(self, (x_axis, y_axis))
